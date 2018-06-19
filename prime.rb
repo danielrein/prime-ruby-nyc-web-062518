8218a905f37dbf240298f35be922d9821753f051
Daniel Ran <danielr1735@gmail.com>
2018-06-19 11:06:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,15 @@
 def prime?(num)
-  case num
-    when num == 0
+  if num == 0
       false
-    when num >= 1 && num <= 3
+  elsif num >= 1 && num <= 3
       true
-    when num > 3
-      factor = 4
-      while factor < num / 2
-        if num / factor == 0
-          false
-        else
-          factor += 1
-      end
+  else
+    factor = 4
+    while factor < num / 2
+      if num / factor == 0
+        false
+      else
+        factor += 1
     end
   end
 end
