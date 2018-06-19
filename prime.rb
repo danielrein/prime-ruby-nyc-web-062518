451252bbef2d26c0b12394b50e69fd3fb3a14c58
Daniel Ran <danielr1735@gmail.com>
2018-06-19 11:02:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(num)
   case num
-  when num == 0 return false
-  when num >= 1 && num <= 3 return true
+  when num == 0 false
+  when num >= 1 && num <= 3 true
   when num > 3
     factor = 4
     while factor < num / 2
